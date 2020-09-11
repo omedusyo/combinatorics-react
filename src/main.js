59e6ee11ab9008f534e683e4dc47e148e195e3e1
Yuriy Dupyn <yuriy.dupyn@gmail.com>
2020-09-11 21:16:36 +0200

    Add my combinatorics library

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import {
+  combinations,
+} from "combinatorics";
+
+console.log(Array.from(combinations([1,2,3,4,5], 2)));
+
 const appDOM = document.getElementById("combinatorics-app");
 
 function App(props) {
