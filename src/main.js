641e2ba85729aecddae2c1465a024f7ea706f4c1
Yuriy Dupyn <yuriy.dupyn@gmail.com>
2020-09-11 20:36:08 +0200

    Add React.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
 
-console.log("hi");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const appDOM = document.getElementById("combinatorics-app");
+
+function App(props) {
+  return (
+    <div>Hello, World!</div>
+  );
+}
+
+ReactDOM.render(<App />, appDOM);
 
